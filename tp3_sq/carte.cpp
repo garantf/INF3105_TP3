0e7b63048b1fa23452bc85ebd0531aebed0730fc
Felix Garant <bd091986@ens.uqam.ca>
2024-04-05 13:27:02 -0400

    First commit

---
+++ tp3_sq/carte.cpp
@@ -0,0 +1,20 @@
+/*  INF3105 - Structures de données et algorithmes
+ *  UQAM / Département d'informatique / TP3
+ *
+ *  Vos noms + codes permanents :
+ */
+
+#include <cstdio>
+#include <limits>
+#include <math.h>
+#include <queue>
+#include <sstream>
+#include "carte.h"
+
+istream& operator >> (istream& is, Carte& carte)
+{
+
+    
+    return is;
+}
+
---
+++ tp3_sq/carte.h
@@ -0,0 +1,26 @@
+/*  INF3105 - Structures de données et algorithmes
+ *  UQAM / Département d'informatique / TP3
+ *
+ *  Vos noms + codes permanents :
+ */
+ 
+#if !defined(_CARTE__H_)
+#define _CARTE__H_
+#include <cassert>
+#include <istream>
+#include <string>
+
+using namespace std;
+
+class Carte{
+  public:
+
+
+  private:
+
+    
+  friend istream& operator >> (istream& is, Carte& carte);
+};
+
+#endif
+
